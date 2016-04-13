3ab99813a9d7c05d90a74009ea895c8cc05d99e2
Md Bulbul Hossen <info247.bulbul@gmail.com>
2016-04-13 10:58:03 +0600

    Update

--- app/src/main/java/ulabcse/awesometodolist/Dashborad.java
+++ app/src/main/java/ulabcse/awesometodolist/Dashborad.java
@@ -14,9 +14,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
 
 public class Dashborad extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ListAdapter todoListAdapter;
+    private TodoListSQLHelper todoListSQLHelper;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +30,6 @@ public class Dashborad extends AppCompatActivity
         setContentView(R.layout.dashborad);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -124,6 +121,8 @@ public class Dashborad extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+
+
 
         } else if (id == R.id.nav_slideshow) {
 
